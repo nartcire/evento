9c41f4da9fa112ba8c3c61b4aa3cd87ac944d27e
Eric Tran <erictran1547@gmail.com>
2024-08-16 19:55:53 -0700

    Finished Event Card

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,30 @@
-export default function EventsPage() {
-  return <div>EventsPage</div>;
+import { EventoEvent } from "@/lib/types";
+import EventsList from "@/components/events-list";
+import H1 from "@/components/h1";
+
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
+
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+  const events: EventoEvent[] = await response.json();
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      <EventsList events={events} />
+    </main>
+  );
 }
---
+++ src/components/events-list.tsx
@@ -0,0 +1,16 @@
+import EventCard from "./event-card";
+import { EventoEvent } from "@/lib/types";
+
+type EventsListProps = {
+  events: EventoEvent[];
+};
+
+export default function EventsList({ events }: EventsListProps) {
+  return (
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
+      {events.map((event) => (
+        <EventCard key={event.id} event={event} />
+      ))}
+    </section>
+  );
+}
