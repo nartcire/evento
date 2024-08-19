2f415f518c74f23e2bfa7c636c7a74906ec0c52c
Eric Tran <erictran1547@gmail.com>
2024-08-18 20:43:26 -0700

    Added in individualized metadata for the title of pages.

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,14 +1,27 @@
 import H1 from "@/components/h1";
 import Image from "next/image";
-import { handleWebpackExternalForEdgeRuntime } from "next/dist/build/webpack/plugins/middleware-plugin";
+import { Metadata } from "next";
 
-type EventPageProps = {
+type Props = {
   params: {
     slug: string;
   };
 };
 
-export default async function EventPage({ params }: EventPageProps) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const slug = params.slug;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
+  return {
+    title: event.name,
+  };
+}
+
+export default async function EventPage({ params }: Props) {
   const slug = params.slug;
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
